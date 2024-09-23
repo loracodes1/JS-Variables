13b683610aafad957d75bd231535a26456975ce0
florence <loracodes3@gmail.com>
2024-09-23 22:12:09 +0300

    final commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let pi=3.14159;
+//=> undefined
 
 let pi="the ratio between a circle's circumference and diameter";
+//=>Uncaught SyntaxError: Identifier 'pi' has already been declared
 console.log(pi)
